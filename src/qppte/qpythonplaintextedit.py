ad5fcdbef54b940a9d18d758c0b299716e9a7790
Dmitri Priimak <priimak@gmail.com>
2026-09-14 16:30:23 -0500

    Initial version of QPythonPlainTextEdit

---
+++ src/qppte/qpythonplaintextedit.py
@@ -0,0 +1,119 @@
+import tree_sitter_python
+from PySide6.QtGui import QTextCharFormat, QTextCursor
+from PySide6.QtWidgets import QPlainTextEdit
+from tree_sitter import Language, Parser, Point, Query, QueryCursor
+
+from qppte.style import STYLES
+
+PY_LANGUAGE = Language(tree_sitter_python.language())
+PYTHON_PARSER = Parser(PY_LANGUAGE)
+HIGHLIGHTER_QUERY = Query(
+    PY_LANGUAGE,
+    """
+        (function_definition
+          name: (identifier) @function_definition)
+
+        (function_definition (identifier) @special_function (#eq? @special_function "__init__"))
+        ("." (identifier) @special_function (#eq? @special_function "__init__"))
+
+        (type) @type
+
+        (class_definition
+          name: (identifier) @class_definition_name)
+
+        (decorator "@" @decorator)  
+        (decorator "@" (identifier)) @decorator 
+
+        (string_start) @string
+        (string_content) @string
+        (string_end) @string
+
+        ["def" "return" "if" "else" "class" "assert" "async" "await" "break" "continue" "del" "elif" 
+         "else" "except" "finally" "for" "global" "lambda" "pass" "raise" "nonlocal" "return" "try" 
+         "while" "yield" "as" "with" "import" "from"] @keyword
+
+
+        (true) @keyword
+        (false) @keyword
+
+        (integer) @number
+        (float) @number
+
+        (call (identifier) @function_call)
+
+        (keyword_argument (identifier) @keyword_argument) 
+
+        ((identifier) @self (#eq? @self "self"))
+
+        (comment) @line_comment
+    """,
+)
+
+
+class QPythonPlainTextEdit(QPlainTextEdit):
+    def __init__(self, /):
+        super().__init__()
+        self.working = False
+
+    def highlight(self) -> None:
+        cursor: QTextCursor = self.textCursor()
+
+        text = self.toPlainText()
+        lines = text.splitlines()
+
+        def get_offset(p: Point) -> int:
+            return sum([len(line) for line in lines[0 : p.row]]) + p.column + p.row
+
+        input_text = text.encode()
+        tree = PYTHON_PARSER.parse(input_text)
+        query_cursor = QueryCursor(HIGHLIGHTER_QUERY)
+        captures = query_cursor.captures(tree.root_node)
+
+        for capture_name in captures:
+            for node in captures[capture_name]:
+                cursor.setPosition(get_offset(node.start_point))
+                cursor.setPosition(get_offset(node.end_point), QTextCursor.MoveMode.KeepAnchor)
+                cursor.setCharFormat(STYLES["default"][capture_name])
+
+    def rehighlight(self):
+        if not self.working:
+            self.working = True
+            cursor: QTextCursor = self.textCursor()
+            cursor.setPosition(0)
+            cursor.movePosition(QTextCursor.MoveOperation.End, QTextCursor.MoveMode.KeepAnchor)
+            cursor.setCharFormat(QTextCharFormat())
+            self.highlight()
+            self.working = False
+
+    def setCode(self, text: str) -> None:
+        self.setPlainText(text)
+        self.highlight()
+        self.textChanged.connect(self.rehighlight)
+
+
+#
+# class TextEditorWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowTitle("Text Editor with Comment Toggle (Ctrl-/)")
+#         self.setGeometry(100, 100, 800, 600)
+#
+#         self.text_edit = QPythonPlainTextEdit()
+#         self.setCentralWidget(self.text_edit)
+#         # Add sample text
+#         sample_text = """
+# class A(object):
+#     def __init__(a: int):
+#         super().__init__()
+#         self.a = a
+#
+# """
+#
+#         self.text_edit.setCode(sample_text)
+#
+#
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     window = TextEditorWindow()
+#     window.show()
+#     sys.exit(app.exec())
---
+++ src/qppte/style.py
@@ -0,0 +1,38 @@
+from PySide6.QtGui import QColor, QFont, QTextCharFormat
+
+
+class TextCharFormat(QTextCharFormat):
+    def __init__(self, foreground_color: str | None = None, background_color: str | None = None,
+                 weight: QFont.Weight = QFont.Weight.Normal, italic=False, underline=False):
+        super().__init__()
+        if foreground_color is not None:
+            self.setForeground(QColor(foreground_color))
+        if background_color is not None:
+            self.setBackground(QColor(background_color))
+        self.setFontWeight(weight)
+        self.setFontItalic(italic)
+        self.setFontUnderline(underline)
+
+STYLES = {
+    "default": {
+        "function_definition": TextCharFormat(foreground_color="#00627A"),
+        "special_function": TextCharFormat(foreground_color="#B200B2"),
+        "keyword": TextCharFormat(foreground_color="#0033B3"),
+        "decorator": TextCharFormat(foreground_color="#9E880D"),
+        "keyword_argument": TextCharFormat(foreground_color="#660099"),
+        "line_comment": TextCharFormat(foreground_color="#8C8C8C", italic=True),
+        "number": TextCharFormat(foreground_color="#1750EB"),
+        "function_call": TextCharFormat(),
+        "string": TextCharFormat(foreground_color="#067D17"),
+        "self": TextCharFormat(foreground_color="#94558D"),
+        "type": TextCharFormat(foreground_color="#660099"),
+        "class_definition_name": TextCharFormat(weight=QFont.Weight.Bold), #
+    }
+}
+
+def foo(a: int) -> None:
+    if a > 0:
+        pass
+    a = True
+
+foo(a = 8)
